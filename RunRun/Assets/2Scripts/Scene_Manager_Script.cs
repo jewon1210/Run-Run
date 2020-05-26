727231884ffad14aca3d86e8c4c8e45cf432e96f
jewon1210 <49775206+jewon1210@users.noreply.github.com>
2020-05-26 22:59:24 +0900

    first scene make
    first scene making & design
    font changed
    with choi
    code by lee

---
+++ RunRun/Assets/2Scripts/Scene_Manager_Script.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;//scenemanage역할
+
+
+
+public class Scene_Manager_Script : MonoBehaviour
+{
+    float delay_check;//화면 넘기기 딜레이 주는 변수
+    float time_check;//클릭시 시간저장
+    bool Change_Scene;//화면 변경 불값
+
+    [SerializeField] GameObject Loading;//로딩화면 생성
+
+    static Scene_Manager_Script uniqueinstance;
+
+    void Awake()
+    {
+        uniqueinstance = this;
+        DontDestroyOnLoad(gameObject);//안사라지게 해줌
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Change_Scene = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        delay_check += Time.deltaTime;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Change_Scene = true;
+            time_check=delay_check;
+        }
+
+        if(delay_check-time_check>=2&&Change_Scene==true)
+        {
+            SceneManager.LoadScene("Lobby");
+            Change_Scene = false;
+        }
+    }
+}
---
+++ RunRun/Assets/2Scripts/Texture_Manager_Script.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class Texture_Manager_Script : MonoBehaviour
+{
+    float time_Check;//시간 체크해주는 변수
+    int num_Text;
+
+    [SerializeField] Text Title_Text;
+    [SerializeField] Text Touch_Text;
+    [SerializeField] Image Background_Image;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time_Check += Time.deltaTime;
+        if (time_Check >= 0.5f)
+        {
+            time_Check = 0;
+            int i = ++num_Text % 2;
+            switch (i)
+            {
+                case 0:
+                    Touch_Text.CrossFadeAlpha(0.0f, 0.5f, true);//알파값 설정
+                    break;
+                case 1:
+                    Touch_Text.CrossFadeAlpha(1.0f, 0.5f, true);
+                    break;
+            }
+        }
+        if(Input.GetMouseButtonDown(0))
+        {
+            Background_Image.CrossFadeAlpha(0.0f, 0.5f, true);
+        }
+    }
+}
