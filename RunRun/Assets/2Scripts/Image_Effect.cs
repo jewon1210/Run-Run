3d48b3b9a795dd0f205efd289868f22d3d9dac52
ChoiYJ97 <50546899+ChoiYJ97@users.noreply.github.com>
2020-05-28 20:40:50 +0900

    5/28 Lobby design
    decorated the background, UI.
    added temporary character.
    added spacebar image and guide arrow.

---
+++ RunRun/Assets/2Scripts/Image_Effect.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Image_Effect : MonoBehaviour
+{
+
+    [SerializeField] Image Spacebar_Effect;
+    [SerializeField] Image Arrow_Effect;
+
+    float time_Check;//시간 체크해주는 변수
+    int num_Text;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        time_Check += Time.deltaTime;
+        if (time_Check >= 0.5f)
+        {
+            time_Check = 0;
+            int i = ++num_Text % 2;
+            switch (i)
+            {
+                case 0:
+                    Spacebar_Effect.CrossFadeAlpha(0.0f, 0.0f, true);//알파값 설정
+                    Arrow_Effect.CrossFadeAlpha(0.0f, 0.0f, true);
+                    break;
+                case 1:
+                    Spacebar_Effect.CrossFadeAlpha(0.7372549f, 0.0f, true);
+                    Arrow_Effect.CrossFadeAlpha(1.0f, 0.0f, true);
+                    break;
+            }
+        }
+    }
+}
