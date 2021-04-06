b31e56dd51dba52897603b49e5b05c087d239831
ChoiYJ97 <50546899+ChoiYJ97@users.noreply.github.com>
2021-04-06 18:07:50 +0900

    Wrote the scripts for Dragon And Knight.
    Moving, Animation etc

---
+++ RunRun/Assets/2Scripts/FairyTale_Scripts/Dragon_Script.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Dragon_Script : MonoBehaviour
+{
+    Animator Dragon_Ani;
+    Transform DragonTrans;
+    Transform KnightTrans;
+    float distance, x, y, z;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Dragon_Ani = gameObject.GetComponent<Animator>();
+        DragonTrans = gameObject.GetComponent<Transform>();
+        KnightTrans = GameObject.Find("Knight_Character").GetComponent<Transform>();
+        distance = Vector2.Distance(KnightTrans.position, DragonTrans.position);
+        x = KnightTrans.position.x;
+        y = KnightTrans.position.y;
+        z = KnightTrans.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Position();
+        if (distance < 10.0f)
+        {
+            ControlAni(1);
+            DragonTrans.Translate(x * Time.deltaTime, y * Time.deltaTime, 0);
+            if (distance < 2.0f)
+                ControlAni(2);
+        }
+    }
+
+    void ControlAni(int num)
+    {
+        Dragon_Ani.SetInteger("Drangon_Ani", num);
+    }
+
+    void Position()
+    {
+        distance = Vector2.Distance(KnightTrans.position, gameObject.transform.position);
+        x = KnightTrans.position.x;
+        y = KnightTrans.position.y;
+        z = KnightTrans.position.z;
+    }
+}
---
+++ RunRun/Assets/2Scripts/FairyTale_Scripts/Knight_Script.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight_Script : MonoBehaviour
+{
+    Transform KnightTrans;
+    Rigidbody2D _rid;
+    Animator Knight_ani;
+    int CurAni; // 0 - idle, 1- walk, 2 - attack,  3 - jumpUp 4- jumpdown
+
+    private void Awake()
+    {
+        KnightTrans = gameObject.GetComponent<Transform>();
+        _rid = gameObject.GetComponent<Rigidbody2D>();
+        Knight_ani = gameObject.GetComponent<Animator>();
+        CurAni = 0;
+    }
+    void Start()
+    {        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            ControlAni(1);
+            KnightTrans.Translate(Vector2.right * Time.deltaTime * 3.0f);
+            KnightTrans.rotation = new Quaternion(0, 180, 0, 0);
+        }
+
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            ControlAni(1);
+            KnightTrans.Translate(Vector2.right * Time.deltaTime * 3.0f);
+            KnightTrans.rotation = new Quaternion(0, 0, 0, 0);
+        }
+
+        else if (Input.GetKey(KeyCode.LeftControl))
+        {
+            ControlAni(2);
+        }
+
+        else if (Input.GetKey(KeyCode.LeftAlt))
+        {
+            _rid.velocity = new Vector2(0, 6);
+        }
+
+        else if (_rid.velocity.y > 0)
+            ControlAni(3);
+
+        else if (_rid.velocity.y < 0)
+            ControlAni(4);
+
+        else
+            ControlAni(0);
+        
+    }
+
+
+    void ControlAni(int i)
+    {
+        CurAni = i;
+        Knight_ani.SetInteger("Knight_ani", CurAni);
+    }
+}
